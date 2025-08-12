92b9abc260102489ee7fc6d70a2ba5e26c37c089
mayageva2 <mayageva7@gmail.com>
2025-08-12 13:10:30 +0300

    Initial empty main branch

--- Engine/src/engine/model/BOp.java
+++
@@ -1,8 +0,0 @@
-package engine.model;
-
-public enum BOp {
-    NEUTRAL,        // v <- v
-    INCREASE,       // v <- v + 1
-    DECREASE,       // v <- v - 1
-    JUMP_NOT_ZERO   // IF v != 0 GO TO Lk
-}
