f36d7d78b29525368d5b8d976e5f5bc46b642e01
mayageva2 <mayageva7@gmail.com>
2025-08-21 17:07:24 +0300

    adding expand for Constant Assignment Instruction

--- src/emulator/logic/instruction/ConstantAssignmentInstruction.java
+++ src/emulator/logic/instruction/ConstantAssignmentInstruction.java
@@ -1,13 +1,17 @@
 package emulator.logic.instruction;
 
 import emulator.logic.execution.ExecutionContext;
+import emulator.logic.expansion.Expandable;
+import emulator.logic.expansion.ExpansionHelper;
 import emulator.logic.label.FixedLabel;
 import emulator.logic.label.Label;
 import emulator.logic.variable.Variable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-public class ConstantAssignmentInstruction extends AbstractInstruction {
+public class ConstantAssignmentInstruction extends AbstractInstruction implements Expandable {
     private final long constantValue;
 
     public ConstantAssignmentInstruction(Variable target, long constantValue) {
@@ -32,6 +36,32 @@ public class ConstantAssignmentInstruction extends AbstractInstruction {
     public Label execute(ExecutionContext context) {
         context.updateVariable(getVariable(), constantValue);
         return FixedLabel.EMPTY;
+    }
+
+    @Override
+    public List<Instruction> expand(ExpansionHelper helper) {
+        List<Instruction> out = new ArrayList<>();
+        Variable var = getVariable();
+
+        Label firstLabel = getLabel();
+        if (firstLabel == null || FixedLabel.EMPTY.equals(firstLabel)) {
+            firstLabel = helper.freshLabel();
+        }
+
+        for (Instruction zi : new ZeroVariableInstruction(var, firstLabel).expand(helper)) {
+            if (zi instanceof AbstractInstruction ai) {
+                ai.setCreatedFrom(this);
+            }
+            out.add(zi);
+        }
+
+        for (long i = 0; i < constantValue; i++) {
+            IncreaseInstruction inc = new IncreaseInstruction(var);
+            inc.setCreatedFrom(this);
+            out.add(inc);
+        }
+
+        return out;
     }
 
     public long getConstantValue() { return constantValue; }
