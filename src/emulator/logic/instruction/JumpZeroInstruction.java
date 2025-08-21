893b3f89a89152349443261b4e861879cf1f78b9
mayageva2 <mayageva7@gmail.com>
2025-08-21 17:20:38 +0300

    adding expand to jump zero

--- src/emulator/logic/instruction/JumpZeroInstruction.java
+++ src/emulator/logic/instruction/JumpZeroInstruction.java
@@ -1,13 +1,17 @@
 package emulator.logic.instruction;
 
 import emulator.logic.execution.ExecutionContext;
+import emulator.logic.expansion.Expandable;
+import emulator.logic.expansion.ExpansionHelper;
 import emulator.logic.label.FixedLabel;
 import emulator.logic.label.Label;
 import emulator.logic.variable.Variable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-public class JumpZeroInstruction extends AbstractInstruction {
+public class JumpZeroInstruction extends AbstractInstruction implements Expandable {
 
     private final Label jzLabel;
 
@@ -27,6 +31,35 @@ public class JumpZeroInstruction extends AbstractInstruction {
     public Label execute(ExecutionContext context) {
         long v = context.getVariableValue(getVariable());
         return (v == 0L) ? jzLabel : FixedLabel.EMPTY;
+    }
+
+    @Override
+    public List<Instruction> expand(ExpansionHelper helper) {
+
+        List<Instruction> out = new ArrayList<>();
+        Variable var =  getVariable();
+
+        Label firstLabel = getLabel();
+        if (firstLabel == null || FixedLabel.EMPTY.equals(firstLabel)) {
+            firstLabel = helper.freshLabel();
+        }
+
+        Label L1 = helper.freshLabel();
+        Label L = jzLabel;
+
+        JumpNotZeroInstruction jnzToL1 = new JumpNotZeroInstruction(var, L1, firstLabel);
+        jnzToL1.setCreatedFrom(this);
+        out.add(jnzToL1);
+
+        GoToLabelInstruction gtl = new GoToLabelInstruction(var, L);
+        gtl.setCreatedFrom(this);
+        out.add(gtl);
+
+        NeutralInstruction neutral = new NeutralInstruction(var, L1);
+        neutral.setCreatedFrom(this);
+        out.add(neutral);
+
+        return out;
     }
 
     public Label getJzLabel() { return jzLabel; }
