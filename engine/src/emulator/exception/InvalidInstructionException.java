fd5631fe79de729ca4541f2d3283bfc51451d953
mayageva2 <mayageva7@gmail.com>
2025-08-25 12:15:01 +0300

    validating opcodes

--- engine/src/emulator/exception/InvalidInstructionException.java
+++ engine/src/emulator/exception/InvalidInstructionException.java
@@ -3,6 +3,11 @@ package emulator.exception;
 import java.util.Map;
 
 public class InvalidInstructionException extends ProgramException {
+
+    public InvalidInstructionException(String opcode, String reason, int index) {
+        super("BAD_OPCODE", "Invalid instruction: " + opcode + " (" + reason + ")", Map.of("opcode", opcode, "index", index));
+    }
+
     public InvalidInstructionException(String name, String reason) {
         super("BAD_INSTRUCTION", "Invalid instruction: " + name + " (" + reason + ")",
                 Map.of("instruction", name));
